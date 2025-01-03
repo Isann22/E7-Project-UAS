b6dee336b5ce72e05c5fbdb2bb441c41f9f36ab7
Tio <tionaza308.com>
2025-01-03 17:27:11 +0700

    OrderController + view

--- resources/views/tournaments/show.blade.php
+++ resources/views/tournaments/show.blade.php
@@ -14,7 +14,7 @@
                 <div class="card text-sm-center">
                     @foreach ($tournament->tickets as $ticket)
                         <div class="card-header" style="border-bottom: 2px solid orange">
-                            <a href=""
+                            <a href="{{ route('orders.index', ['ticket_id' => $ticket->id])}}"
                                 class="primary-button mx-auto">Buy
                                 Ticket</a>
                         </div>
